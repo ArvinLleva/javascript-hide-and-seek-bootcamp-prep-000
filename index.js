cf9e2b22109b93e6d7490e614b9f85cbfc3468a7
ArvinLleva <arvin.lleva@yahoo.com>
2018-06-15 21:44:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,6 +17,23 @@ function increaseRankBy(n){
 
 
 function deepestChild(){
-  var grandNode = document.querySelector('#grand-node').querySelectorAll('div'); // this gets all the div's child nodes
-  return grandNode[grandNode.length-1] // i used this to return the deepest child after tinkering with the outputs in the console
+
+ //My first way of completing this task without using the breadth-first search
+ var grandNode = document.querySelector('#grand-node').querySelectorAll('div'); // this gets all the div's child nodes
+ 
+  return grandNode[grandNode.length-1]; // i used this to return the deepest child after tinkering with the outputs in the console
+  
+//another way of creating this function
+/* 
+var node = document.queryselector('#grand-node');
+var nextNode = node.children[0];
+
+while(nextNode){ // this checks if there's a child of that node, this is another good way of iterating through something
+  node = nextNode; // this gets the child of that current node  
+  nextNode = node.children[0]; // this goes to the next node 
 }
+return node // this returns the deepest child from the grandNode ID element
+*/
+}
+
+
